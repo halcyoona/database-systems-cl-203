d957f5c9ccc5fc8aced08ef56b899f73abeb3ce7
halcyoona <halcyoona@gmail.com>
2019-04-02 06:15:29 +0500

    data

---
+++ labtask-03/question2/question2-data.sql
@@ -0,0 +1,27 @@
+INSERT INTO EMPLOYEE
+	(EMP_ID, FIRSTNAME, LASTNAME, EMAIL, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPT_ID)
+	VALUES
+	(100, 'Steven', 'King', 'SKING', '1987-6-17', 'AD_PRES', 24000, 0, 90), 
+	(101, 'Neena', 'Kochhar', 'NKOCHHAR', '1987-6-18', 'AD_VP', 17000, 100, 90),
+	(102, 'Lex', 'De Haan', 'LDEHAAN', '1987-6-19', 'AD_VP', 17000, 100, 90),
+	(103, 'Alexander', 'Hunold', 'AHUNOLD', '1987-6-20', 'IT_PROG', 9000, 102, 60),
+	(104, 'Bruce', 'Ernst', 'BERNST', '1987-6-21', 'IT_PROG', 6000, 103, 60),
+	(105, 'David', 'Austin', 'DAUSTIN', '1987-6-22', 'IT_PROG', 48000, 103, 60),
+	(106, 'Valli', 'Pataballa', 'VPATABALA', '1987-6-23', 'IT_PROG', 48000, 103, 60),
+	(107, 'Diana', 'Lorentz', 'DLORENTZ', '1987-6-24', 'IT_PROG', 42000, 101, 60),
+	(108, 'Nancy', 'Greenberg', 'NGREENBERG', '1987-6-25', 'FI_MGR', 12000, 108, 100),
+	(109, 'Daniel', 'Faviet', 'DFAVIET', '1987-6-26', 'FI_ACCOUNT', 9000, 108, 100),
+	(110, 'John', 'Chen', 'JCHEN', '1987-6-27', 'FI_ACCOUNT', 8200, 108, 100),
+	(111, 'Ismael', 'Sciarra', 'ISCIARRA', '1987-6-28', 'FI_ACCOUNT', 7700, 108, 100),
+	(112, 'Jose Manue', 'Urman', 'JURMAN', '1987-6-29', 'FI_ACCOUNT', 7800, 108, 100),
+	(113, 'Luis', 'POPP', 'LPOPP', '1987-6-30', 'PU_ACCOUNT', 6900, 100, 100),
+	(114, 'Den', 'Raphaely', 'DRAPHAEL', '1987-7-1', 'PU_MAN', 11000, 114, 30),
+	(115, 'Alexander', 'Khoo', 'AKHOO', '1987-7-2', 'PU_CLARK', 3100, 114, 30),
+	(116, 'Sheli', 'Baida', 'SBAIDA', '1987-7-3', 'PU_CLARK', 2900, 114, 30),
+	(117, 'Sigal', 'Tobias', 'STOBIAS', '1987-7-4', 'PU_CLARK', 2800, 114, 30),
+	(118, 'Guy', 'Himuro', 'GHIMURO', '1987-7-5', 'PU_CLARK', 2600, 114, 30),
+	(119, 'Karen', 'Colmenares', 'KCOLMENA', '1987-7-6', 'PU_CLARK', 2500, 114, 30),
+	(120, 'Matthew', 'Weiss', 'MWEISS', '1987-7-7', 'ST_MAN', 8000, 100, 50),
+	(121, 'Adam', 'Fripp', 'AFRIPP', '1987-7-8', 'ST_MAN', 8200, 100, 50),
+	(122, 'Payam', 'Kaufling', 'PKAULIN', '1987-7-9', 'ST_MAN', 8000, 100, 50),
+	(123, 'Shanta', 'Vollman', 'SVOLLMAN', '1987-7-10', 'ST_MAN', 7900, 100, 50);
